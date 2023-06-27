26367e4aa5fc6330cdd3185003fefbe228ec2ad0
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-26 22:35:55 -0400

    Added starter code to seeds

--- seeds/employer.js
+++ seeds/employer.js
@@ -1,1 +1,20 @@
-const { EmployerUser } = require('../models');
+const { EmployerUser } = require('../models/EmployerUser');
+const { sequelize } = require('../models');
+
+const employerData = [
+    {
+        company: '',
+        first_name: '',
+        last_name: '',
+        role: '',
+        email: '',
+        password: '',
+        },
+];
+
+const seedEmployer = async () => {
+    await sequelize.sync({ force: true });
+    await EmployerUser.bulkCreate(employerData);
+};
+
+module.exports = seedEmployer;
--- seeds/technology.js
+++ seeds/technology.js
@@ -1,9 +1,20 @@
-const { Technology } = require('../models');
+const { Technology } = require('../models/Technology');
+const { sequelize } = require('../models');
 
 const technologyData = [
     {
-
-    }
+        name: 'JavaScript',
+        badge_link: 'https://example.com',
+    },
+    {
+        name: 'Python',
+        badge_link: 'https://example.com',
+    },
+    {
+        name: 'C++',
+        badge_link: 'https://example.com',
+    },
+    // Ask group what else we want to add
 ];
 
 const seedTechnology = () => Technology.bulkCreate(technologyData);
