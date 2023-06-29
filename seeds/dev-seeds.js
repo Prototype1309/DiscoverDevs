289f2e05afa1fd15bed76550a438a66940414b99
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-29 12:39:25 -0400

    Added addtl data to seeds & more routes

--- seeds/dev-seeds.js
+++ seeds/dev-seeds.js
@@ -9,6 +9,16 @@ const developerData = [
     password: '12345',
     picture_link: 'http://fakeimglink.com/',
   },
+  {
+    first_name: 'Eliza',
+    last_name: 'Davis',
+    yrs_experience: 8,
+    technology_id: 1,
+    location: 'Raleigh',
+    email: 'ElizaMDavis@teleworm.us',
+    password: '123454321',
+    picture_link: 'http://fakeimglink.com/',
+  },
 ];
 
 module.exports = developerData;
