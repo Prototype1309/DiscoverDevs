c63f52056e086136be03ed88c4a8ca288ce8a0f0
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-29 12:26:13 -0400

    added starter to devUser route

--- controllers/api/devUser.js
+++ controllers/api/devUser.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const DevUser = require('../../models/DevUser');
 
-
+router.get('/', async (req, res) => {
+  try {
+    const devUser = await DevUser.findAll({});
+    res.status(200).json(devUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
