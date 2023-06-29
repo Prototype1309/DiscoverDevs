f65114da4fa90612ff8fc2278e60f739d9cb0c0a
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-29 13:14:46 -0400

    Added get by id in Tech

--- controllers/api/devUser.js
+++ controllers/api/devUser.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { DevUser, Technology} = require('../../models');
+const { DevUser, Technology } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
