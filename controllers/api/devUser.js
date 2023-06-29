12b36879d2cf17f9e6c51fca8184159b84d61535
obelisk477 <cwbatte2@gmail.com>
2023-06-29 18:11:46 -0400

    Added capability to seed association through-table

--- controllers/api/devUser.js
+++ controllers/api/devUser.js
@@ -1,5 +1,24 @@
 const router = require('express').Router();
+const { DevUser, Technology } = require('../../models')
 
+router.get('/', async (req, res) => {
+    try {
+        const devUsers = await DevUser.findAll({
+            include: {
+                model: Technology
+            }
+        })
+
+        const simpleData = devUsers.map((event) => event.get({ plain: true }))
+
+        console.log(simpleData)
+        res.status(200).json(simpleData)
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 -- Dropping database and creating new database
-DROP DATABASE IF EXISTS developdevs_db;
+DROP DATABASE IF EXISTS discoverdevs_db;
 CREATE DATABASE discoverdevs_db;
 USE discoverdevs_db;
