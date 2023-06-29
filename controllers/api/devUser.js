049cfca9c82f6155e9798b0c0e5f1aa4b737fb1e
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-29 19:44:35 -0400

    Got post route to work!

--- controllers/api/devUser.js
+++ controllers/api/devUser.js
@@ -1,24 +1,23 @@
 const router = require('express').Router();
-const { DevUser, Technology } = require('../../models')
+const { DevUser, Technology, UserTech } = require('../../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const devUsers = await DevUser.findAll({
-            include: {
-                model: Technology
-            }
-        })
+  try {
+    const devUsers = await DevUser.findAll({
+      include: {
+        model: Technology,
+      },
+    });
 
-        const simpleData = devUsers.map((event) => event.get({ plain: true }))
+    const simpleData = devUsers.map((event) => event.get({ plain: true }));
 
-        console.log(simpleData)
-        res.status(200).json(simpleData)
-
-    } catch (err) {
-        console.error(err)
-        res.status(500).json(err)
-    }
-})
+    console.log(simpleData);
+    res.status(200).json(simpleData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 //Do we need to add a way to get user by tech_id here?
 router.get('/:id', async (req, res) => {
@@ -39,7 +38,16 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
+    const tech = req.body.technologies;
+    delete req.body.technologies;
     const postDev = await DevUser.create(req.body);
+
+    const insertTech = tech.map((singleTech) => {
+      return { developerId: postDev.id, technologyId: singleTech };
+    });
+    console.log(insertTech);
+    const devTech = await UserTech.bulkCreate(insertTech);
+
     res.status(200).json(postDev);
   } catch (err) {
     console.error(err);
--- seeds/through-seeds.js
+++ seeds/through-seeds.js
@@ -1,8 +1,8 @@
 const throughData = [
-    {
-        developerId: 1, 
-        technologyId: 1,
-    },
-]
+  {
+    developerId: 1,
+    technologyId: 1,
+  },
+];
 
 module.exports = throughData
