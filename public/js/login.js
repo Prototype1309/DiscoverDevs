2b71a386cff9fa1b0c6add8bbbd170cb0a090c3f
prototype1309 <simo.hourri@gmail.com>
2023-07-02 19:53:41 -0400

    added more options for registration page, transparent scrollbat, dropdown option for tech, liked all buttons together

--- public/js/login.js
+++
@@ -1,5 +0,0 @@
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
-const wrapper = document.querySelector('.wrapper');
-signUpBtnLink.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
-});
--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,1 @@
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const wrapper = document.querySelector('.wrapper');
-signInBtnLink.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
-});
+
