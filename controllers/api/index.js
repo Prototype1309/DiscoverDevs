2601f3118e1283e11ac5faead8c6d4a8ba3e7ebe
prototype1309 <simo.hourri@gmail.com>
2023-07-02 02:46:33 -0400

    home routs fixiing merge

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const userRoutes = require('./userRoutes');
-
-router.use('/users', userRoutes);
-=======
 const devApiRoute = require('./devUser');
 const empApiRoute = require('./employerUser');
 const techApiRoute = require('./technology');
@@ -11,6 +6,5 @@ const techApiRoute = require('./technology');
 router.use('/devUser', devApiRoute);
 router.use('/employerUser', empApiRoute);
 router.use('/technology', techApiRoute);
->>>>>>> 37dc5b05fcd2e152340af1f18e410e7d8b5e7a20
 
 module.exports = router;
