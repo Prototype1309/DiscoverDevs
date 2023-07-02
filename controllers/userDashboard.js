3bee967aae38249b46834df281b1f81291ccdcee
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-07-02 15:21:43 -0400

    Added GET to userDashboard

--- controllers/userDashboard.js
+++ controllers/userDashboard.js
@@ -1,5 +1,33 @@
-const router = require('express').Router();
-const DevUser = requrie('../models/DevUser');
+const express = require('express');
+const router = express.Router();
+const DevUser = require('./models/DevUser');
+const Technology = require('./models/Technology');
+
+router.use(express.urlencoded({ extended: true }));
+
+router.get('/dev/:id', async (req, res) => {
+  const devId = req.params.id;
+  try {
+    const dev = await DevUser.findByPk(devId, {
+      include: [
+        {
+          model: Technology,
+          attributes: ['id', 'name', 'badge_link'],
+        },
+      ],
+    });
+    if (!dev) {
+      return res
+        .status(404)
+        .render('error', { error: 'User profile not found.' });
+    }
+
+    const devData = dev.get({ plain: true });
+    res.render('devs', { devData });
+  } catch (error) {
+    res.status(500).render('error', { error: 'Internal server error.' });
+  }
+});
 
 router.post('/dev/:id', async (req, res) => {
   const devId = req.params.id;
