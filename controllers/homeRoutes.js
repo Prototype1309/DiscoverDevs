8142ec526e5459891cb36934a08323f3951b13e8
prototype1309 <simo.hourri@gmail.com>
2023-07-05 18:24:18 -0400

    added Confirm Pasword on both registration page employer

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Technology = require('../models/Technology');
 
 router.get('/', (req, res) => {
-  res.render('login', {
+  res.render('home', {
     signedIn: req.session.loggedIn
   });
 });
@@ -27,6 +27,17 @@ router.get('/register', async (req, res) => {
     signedIn: req.session.loggedIn
   });
 });
+
+router.get('/register', async (req, res) => {
+  const listOfTechs = await Technology.findAll({});
+  const techs = listOfTechs.map((tech) => tech.get({ plain: true }));
+
+  res.render('register', {
+    techs,
+    signedIn: req.session.loggedIn,
+  });
+});
+
 
 router.get('/employer', (req, res) => {
   res.render('employer', {
