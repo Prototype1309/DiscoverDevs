b7b034e6f01cf6cee660a4293a28ab87031e7ff4
prototype1309 <simo.hourri@gmail.com>
2023-07-05 21:25:35 -0400

    rebase commit

--- public/js/employer.js
+++ public/js/employer.js
@@ -6,8 +6,8 @@ registrationForm.addEventListener('submit', async (e) => {
   const [
     reqFirstName,
     reqLastName,
-    reqCompanyName,
     reqEmail,
+    reqCompanyName,
     reqPassword,
     reqRole,
   ] = [
@@ -16,10 +16,17 @@ registrationForm.addEventListener('submit', async (e) => {
     e.target[2].value,
     e.target[3].value,
     e.target[4].value,
-    e.target[5].value,
-    e.target[6].value,
-  ];
+    e.target[5].value
 
+  ];
+  console.log(reqFirstName);
+  console.log(reqLastName);
+  console.log(reqCompanyName);
+  console.log(reqEmail);
+  console.log(reqPassword);
+  console.log(reqRole);
+  
+  
   const createNewEmployer = await fetch('/api/employerUser', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
